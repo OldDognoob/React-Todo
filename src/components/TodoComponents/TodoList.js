a6d54e8e250c99c72d19fd8bcd8db35f2e5f9604
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-10 19:58:43 +0200

    refactor and correct todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,24 +1,14 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
 
-import React from 'react';
-
-import Todo from './Todo';
+import Todo from "./Todo";
 
 const TodoList = props =>{
     return(
-        <div className="todo-list">
-            {props.todos.map(todo =>(
-                <Todo 
-                key={todo.id}
-                todo={todo}
-                togglePurchased={props.togglePurchased}
-                />
-            ))}
-            <button className="clear-btn" onClick={props.clearPurchased}>
-                Clear Purchased
-            </button>
-        </div>
+      <div className="todo-list">
+         {props.groceries.map(item => (
+        <Todo key={item.id} item={item} toggleItem={props.toggleItem} />
+      ))}
+      </div>
     );
 };
 
