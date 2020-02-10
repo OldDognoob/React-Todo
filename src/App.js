497d86fd8dd41428ab5e7085ab9188ba36123a45
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-10 16:13:19 +0200

    creating a todolist of data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,52 @@ import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
 
+
+const todosList =[
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+   constructor(){
+     super();
+     this.state={
+       //the state in class component has a key in todoList that is the value of all the todos
+       todos: todosList,
+       name:""
+
+     };
+   }
+
+   togglePurchased=() =>{
+     const newTodoList = {
+       ...this.state,
+       todosList: ''
+     };
+   };
+
+   //add a new item
+
+
+
+
+
+
+
   render() {
     return (
       <div>
